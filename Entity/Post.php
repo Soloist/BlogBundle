c9bfe8bf8bb361894f12588cc43cba2739ba35c3
yohang <yohan@giarelli.org>
2012-06-22 16:10:08 +0200

    Revert "Removed translatable feature"

--- Entity/Post.php
+++ Entity/Post.php
@@ -59,6 +59,11 @@ class Post implements CrudableInterface
      */
     protected $images;
 
+    /**
+     * @var string
+     */
+    protected $locale;
+
     public function __construct()
     {
         $this->publishedAt = new \DateTime();
@@ -235,5 +240,14 @@ class Post implements CrudableInterface
         }
 
         return null;
+    }
+
+    /**
+     * Translatable setter for doctrine extensions
+     * @param string $locale
+     */
+    public function setTranslatableLocale($locale)
+    {
+        $this->locale = $locale;
     }
 }
